3b8c480cd6049f126c1e4c67dc1bd5bb4553de7b
herrqian <qianjing1993@gmail.com>
2019-10-24 22:34:36 +0200

    the length of row and col can be set

--- src/main/scala/de/htwg/se/connect_four/model/Matrix.scala
+++ src/main/scala/de/htwg/se/connect_four/model/Matrix.scala
@@ -1,9 +1,11 @@
 package de.htwg.se.connect_four.model
 
 case class Matrix[T] (rows:Vector[Vector[T]]) {
-  def this(size:Int, filling:T) = this(Vector.tabulate(size, size){(row, col) => filling})
-  val size:Int = rows.size
+  def this(row:Int, col:Int, filling:T) = this(Vector.tabulate(row, col){(row, col) => filling})
+  val row:Int = rows.size
+  val col :Int = rows.head.size
+  val size:Int = row * col
   def cell(row:Int, col:Int):T = rows (row)(col)
-  def fill (filling:T):Matrix[T]= copy( Vector.tabulate(size, size){(row, col) => filling})
+  def fill (filling:T):Matrix[T]= copy( Vector.tabulate(row, col){(row, col) => filling})
   def replaceCell(row:Int, col:Int, cell:T):Matrix[T] = copy(rows.updated(row, rows(row).updated(col, cell)))
 }

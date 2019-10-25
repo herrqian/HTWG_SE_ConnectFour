aaa2bef8b3bb8b9702c0c2cae9e64e0235ae1375
herrqian <qianjing1993@gmail.com>
2019-10-25 15:23:47 +0200

    added input with player name

--- src/main/scala/de/htwg/se/connect_four/ConnectFour.scala
+++ src/main/scala/de/htwg/se/connect_four/ConnectFour.scala
@@ -3,16 +3,17 @@ package de.htwg.se.connect_four
 import de.htwg.se.connect_four.aview.Tui
 import de.htwg.se.connect_four.model.{Grid, Player}
 
-import scala.io.StdIn
+import scala.io.StdIn.readLine
 
 object ConnectFour {
-  import scala.io.StdIn.readLine
-    var grid = new Grid(7,6)
+    var grid = new Grid(6,7)
     val tui = new Tui
 
     def main(args: Array[String]): Unit = {
       var input: String = ""
-
+      println("Please give your name:")
+      input = readLine()
+      println(s"$input, welcome to Connect Four")
       do {
         println(grid.toString)
         input = readLine()

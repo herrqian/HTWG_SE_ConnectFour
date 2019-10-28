f08de3eecff063bceef61dccf22cf02e5b740531
herrqian <qianjing1993@gmail.com>
2019-10-28 09:45:11 +0100

    added player into main

--- src/main/scala/de/htwg/se/connect_four/ConnectFour.scala
+++ src/main/scala/de/htwg/se/connect_four/ConnectFour.scala
@@ -2,18 +2,18 @@ package de.htwg.se.connect_four
 
 import de.htwg.se.connect_four.aview.Tui
 import de.htwg.se.connect_four.model.{Grid, Player}
+import scala.io.StdIn.readLine
 
 object ConnectFour {
-  import scala.io.StdIn.readLine
     var grid = new Grid(6,7)
     val tui = new Tui
 
     def main(args: Array[String]): Unit = {
       var input: String = ""
       println("Please give your name:")
-      input = readLine()
-      val player1 = Player(input)
+      val player1 = Player(readLine())
       println(s"$player1, welcome to Connect Four")
+
       do {
         println(grid.toString)
         input = readLine()

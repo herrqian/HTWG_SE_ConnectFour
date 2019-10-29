20b22dbbdf15eeeec101258438689fa9f5cfc4f3
Vidatschi <reifbeg@gmail.com>
2019-10-29 17:15:22 +0100

    added some tui things

--- src/main/scala/de/htwg/se/connect_four/ConnectFour.scala
+++ src/main/scala/de/htwg/se/connect_four/ConnectFour.scala
@@ -3,6 +3,7 @@ package de.htwg.se.connect_four
 import de.htwg.se.connect_four.aview.Tui
 import de.htwg.se.connect_four.model.{Grid, Player}
 import scala.io.StdIn.readLine
+import util.control.Breaks._
 
 object ConnectFour {
     var grid = new Grid(6,7)
@@ -10,14 +11,30 @@ object ConnectFour {
 
     def main(args: Array[String]): Unit = {
       var input: String = ""
-      println("Please give your name:")
+      println("Player 1 please type in your name:")
       val player1 = Player(readLine())
-      println(s"$player1, welcome to Connect Four")
+      var input2: String = ""
+      println("Player 2 please type in your name:")
+      val player2 = Player(readLine())
+      println(s"$player1 and $player2, Welcome to Connect Four")
 
       do {
+        if (tui.getTurn(0)) {
+          println(s"$player1, its your turn!")
+        } else if (tui.getTurn(1)) {
+          println(s"$player2, its your turn!")
+        }
         println(grid.toString)
         input = readLine()
-        grid = tui.processInputLine(input, grid)
+        val result = tui.processInputLine(input, grid)
+        grid = result._1
+        val flag = result._2
+        if (flag != (-1, -1) && input != "n") {
+          if (tui.checkWinner(grid, flag._1, flag._2)) {
+            println("you win!")
+            input = "n"
+          } else {tui.changeTurn()}
+        }
       } while (input != "q")
     }
 }
--- src/main/scala/de/htwg/se/connect_four/model/Grid.scala
+++ src/main/scala/de/htwg/se/connect_four/model/Grid.scala
@@ -1,5 +1,7 @@
 package de.htwg.se.connect_four.model
 
+import scala.collection.mutable.ArrayBuffer
+
 case class Grid(cells: Matrix[Cell]) {
   def this(row: Int, col:Int) = this(new Matrix[Cell](row, col, Cell(0)))
   val size: Int = cells.size
@@ -8,9 +10,43 @@ case class Grid(cells: Matrix[Cell]) {
   def row(row: Int):House=House(cells.rows(row))
   def col(col:Int):House=House(cells.rows.map(row=>row(col)))
 
+  def link_diagonal(row:Int, col:Int):House = {
+    var mrow = row-1
+    var mcol = col-1
+    val mvec = ArrayBuffer[Cell]()
+    while (mrow < cells.row && mcol > 0) {
+      mrow = mrow + 1
+      mcol = mcol - 1
+    }
+    while (mrow >= 0 && mcol < cells.col) {
+      mvec.append(cells.cell(mrow, mcol))
+      mrow = mrow - 1
+      mcol = mcol + 1
+    }
+    House(mvec.toVector)
+  }
+
+
+  def right_diagonal(row: Int, col: Int):House = {
+    var mrow = row-1
+    var mcol = col-1
+    val mvec = ArrayBuffer[Cell]()
+    while (mrow > 0 && mcol > 0) {
+      mrow = mrow - 1
+      mcol = mcol - 1
+    }
+    while (mrow < cells.row && mcol < cells.col) {
+      mvec.append(cells.cell(mrow, mcol))
+      mrow = mrow + 1
+      mcol = mcol + 1
+    }
+    House(mvec.toVector)
+  }
+
   override def toString: String = cells.toString
 }
 
 case class House(private val cells:Vector[Cell]) {
   def cell(index:Int):Cell=cells(index)
+  def getCells: Vector[Cell] = cells
 }

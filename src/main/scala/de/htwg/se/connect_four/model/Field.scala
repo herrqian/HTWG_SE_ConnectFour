113d70acb5854540c7db2b3ea64d63ac3d0cd38d
herrqian <qianjing1993@gmail.com>
2019-11-13 15:48:26 +0100

    improved code coverage

--- src/main/scala/de/htwg/se/connect_four/model/Field.scala
+++ src/main/scala/de/htwg/se/connect_four/model/Field.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.connect_four.model
 
-case class Field(private val cells:Vector[Cell]) {
+case class Field(cells:Vector[Cell]) {
   def cell(index:Int):Cell=cells(index)
   def getCells: Vector[Cell] = cells
 }

73a771d48cec84a892a7e342dad464ac6d6dfbae
herrqian <qianjing1993@gmail.com>
2019-11-14 17:31:41 +0100

    modified controller class

--- src/main/scala/de/htwg/se/connect_four/aview/Tui.scala
+++ src/main/scala/de/htwg/se/connect_four/aview/Tui.scala
@@ -1,107 +1,96 @@
 package de.htwg.se.connect_four.aview
 
-import de.htwg.se.connect_four.model.{Cell, Grid}
 import de.htwg.se.connect_four.controller.Controller
 import de.htwg.se.connect_four.util.Observer
 
 class Tui(controller: Controller) extends Observer{
 
   controller.add(this)
-  //todo:continue to code
-  var playerList = Array(true, false)
+  var winnercheck = false
 
-  def processInputLine(input: String, grid: Grid): (Grid, (Int, Int)) = {
+  def processInputLine(input: String): Unit = {
     input match {
-      case "q" => (grid, (0, 0))
+      case "q" => println("The game exit")
       case "n small" =>
-        playerList = Array(true, false)
-        (new Grid(6, 7), (0, 0))
+        controller.playerList = Array(true, false)
+        controller.createEmptyGrid(6,7)
       case "n middle" =>
-        playerList = Array(true, false)
-        (new Grid(10,11),(0,0))
+        controller.playerList = Array(true, false)
+        controller.createEmptyGrid(10,11)
       case "n huge" =>
-        playerList = Array(true,false)
-        (new Grid(16, 17), (0,0))
+        controller.playerList = Array(true,false)
+        controller.createEmptyGrid(16,17)
       case _ =>
         input.toList.filter(c => c != ' ') match {
           case 'i' :: column :: Nil =>
-            var value = 0
-            if (playerList(0)) {
-              value = 1
-            } else {
-              value = 2
-            }
-            if (column.asDigit > grid.cells.col - 1) {
-              println("wrong input, repeat your turn!")
-              return (grid, (-1, -1))
-            }
-            val checkGrid = checkBottom(grid, column.asDigit, value)
-            if (checkGrid._2 == -1) {
+            val row_number = controller.setBottom(column.asDigit)
+            if (row_number == -1) {
               println("cant insert at this column, repeat your turn!")
-              return (grid, (-1, -1))
             }
-            (checkGrid._1, (checkGrid._2, column.asDigit))
+            if (controller.checkWinner(row_number, column.asDigit)) {
+              printf("The player%s is the winner!\n", controller.currentPlayer().toString)
+              winnercheck = true
+            } else {
+              controller.changeTurn()
+            }
           case _ =>
             println("wrong input, repeat your turn!")
-            (grid, (-1, -1))
         }
     }
   }
-
-
-  def checkBottom(grid: Grid, column: Int, value: Int): (Grid, Int) = {
-    for (i <- grid.cells.row - 1 to 0 by -1) {
-      if (grid.col(column).cell(i).equals(Cell(0))) {
-        return (grid.set(i, column, value), i)
-      }
-    }
-    (grid, -1)
-  }
-
-  def checkWinner(grid: Grid, row: Int, col: Int): Boolean = {
-    if (check4number(grid.col(col).getCells)) {
-      true
-    } else if (check4number(grid.row(row).getCells)) {
-      true
-    } else if (check4number(grid.link_diagonal(row, col).getCells)) {
-      true
-    } else if (check4number(grid.right_diagonal(row, col).getCells)) {
-      true
-    } else {
-      false
-    }
-  }
-
-  def check4number(vector: Vector[Cell]): Boolean = {
-    var counter = 0
-    for (cell <- vector) {
-      if (cell.equals(Cell(currentPlayer()))) {
-        counter = counter + 1
-        if (counter == 4) {
-          return true
-        }
-      } else {
-        counter = 0
-      }
-    }
-    false
-  }
-
-  def getTurn(playerNumber: Int): Boolean = {
-    playerList(playerNumber)
-  }
-
-  def changeTurn(): Unit = {
-    playerList(0) = !playerList(0)
-    playerList(1) = !playerList(1)
-  }
-
-  def currentPlayer(): Int = {
-    if (playerList(0)) {
-      return 1
-    }
-    2
-  }
-
   override def update(): Unit = println(controller.gridToString)
+
+//  def checkBottom(grid: Grid, column: Int, value: Int): (Grid, Int) = {
+//    for (i <- grid.cells.row - 1 to 0 by -1) {
+//      if (grid.col(column).cell(i).equals(Cell(0))) {
+//        return (grid.set(i, column, value), i)
+//      }
+//    }
+//    (grid, -1)
+//  }
+//
+//  def checkWinner(grid: Grid, row: Int, col: Int): Boolean = {
+//    if (check4number(grid.col(col).getCells)) {
+//      true
+//    } else if (check4number(grid.row(row).getCells)) {
+//      true
+//    } else if (check4number(grid.link_diagonal(row, col).getCells)) {
+//      true
+//    } else if (check4number(grid.right_diagonal(row, col).getCells)) {
+//      true
+//    } else {
+//      false
+//    }
+//  }
+//
+//  def check4number(vector: Vector[Cell]): Boolean = {
+//    var counter = 0
+//    for (cell <- vector) {
+//      if (cell.equals(Cell(currentPlayer()))) {
+//        counter = counter + 1
+//        if (counter == 4) {
+//          return true
+//        }
+//      } else {
+//        counter = 0
+//      }
+//    }
+//    false
+//  }
+//
+//  def getTurn(playerNumber: Int): Boolean = {
+//    playerList(playerNumber)
+//  }
+//
+//  def changeTurn(): Unit = {
+//    playerList(0) = !playerList(0)
+//    playerList(1) = !playerList(1)
+//  }
+//
+//  def currentPlayer(): Int = {
+//    if (playerList(0)) {
+//      return 1
+//    }
+//    2
+//  }
 }

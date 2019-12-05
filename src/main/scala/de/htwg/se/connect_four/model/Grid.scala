bfac99db929d11aa9a175addb0c9fcc3604446f6
herrqian <qianjing1993@gmail.com>
2019-12-05 22:13:17 +0100

    adapt the grid class from gridinterface

--- src/main/scala/de/htwg/se/connect_four/model/Grid.scala
+++ src/main/scala/de/htwg/se/connect_four/model/Grid.scala
@@ -2,7 +2,7 @@ package de.htwg.se.connect_four.model
 
 import scala.collection.mutable.ArrayBuffer
 
-case class Grid(cells: Matrix[Cell]) {
+case class Grid(cells: Matrix[Cell]) extends GridInterface {
   def this(row: Int, col:Int) = this(new Matrix[Cell](row, col, Cell(0)))
   val size: Int = cells.size
   def cell(row:Int, col: Int): Cell=cells.cell(row,col)

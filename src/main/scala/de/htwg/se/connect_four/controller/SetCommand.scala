14f3b3638e9e6d7c717133753dd16e192ee9a98c
herrqian <qianjing1993@gmail.com>
2019-12-12 23:25:16 +0100

    modified setcommand class

--- src/main/scala/de/htwg/se/connect_four/controller/SetCommand.scala
+++ src/main/scala/de/htwg/se/connect_four/controller/SetCommand.scala
@@ -3,10 +3,18 @@ package de.htwg.se.connect_four.controller
 import de.htwg.se.connect_four.util.Command
 
 class SetCommand(row: Int, col:Int, value:Int, controller: Controller) extends  Command {
-  override def doStep: Unit =  controller.grid = controller.grid.set(row,col,value)
+  override def doStep: Unit = {
+    controller.grid = controller.grid.set(row,col,value)
+  }
 
-  override def undoStep: Unit = controller.grid = controller.grid.set(row, col, 0)
+  override def undoStep: Unit = {
+    controller.grid = controller.grid.set(row, col, 0)
+    controller.changeTurn()
+  }
 
-  override def redoStep: Unit = controller.grid = controller.grid.set(row, col, value)
+  override def redoStep: Unit = {
+    controller.grid = controller.grid.set(row, col, value)
+    controller.changeTurn()
+  }
 
 }

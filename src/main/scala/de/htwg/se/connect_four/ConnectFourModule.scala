c281aec8bb6a30e8e8ab900683ce83784efc8e29
herrqian <qianjing1993@gmail.com>
2020-01-07 21:56:45 +0100

    inserted a connectfourModule unfinished

---
+++ src/main/scala/de/htwg/se/connect_four/ConnectFourModule.scala
@@ -0,0 +1,11 @@
+package de.htwg.se.connect_four
+
+import com.google.inject.AbstractModule
+import com.google.inject.name.Names
+import net.codingwell.scalaguice.ScalaModule
+import de.htwg.se.connect_four.controller.controllerComponent._
+import de.htwg.se.connect_four.model.gridComponent.GridInterface
+
+class ConnectFourModule {
+
+}
